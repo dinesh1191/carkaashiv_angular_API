e7214a263800452efd1e51e79abe6d44a6a9a508
dinesh_v <dinesh.varadhan10@gmail.com>
2026-01-08 18:48:50 +0530

    fix: ef table migration settup added

---
+++ Migrations/20260107170235_InitialCreate_Postgres.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace carkaashiv_angular_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate_Postgres : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "tbl_cart",
+                columns: table => new
+                {
+                    cart_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    u_id = table.Column<int>(type: "integer", nullable: false),
+                    part_id = table.Column<int>(type: "integer", nullable: false),
+                    quantity = table.Column<int>(type: "integer", nullable: false),
+                    added_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_cart", x => x.cart_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_emp",
+                columns: table => new
+                {
+                    emp_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    emp_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    emp_phone = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    emp_email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    emp_role = table.Column<string>(type: "character varying(10)", maxLength: 10, nullable: false),
+                    emp_pass = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    idt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_emp", x => x.emp_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_order_items",
+                columns: table => new
+                {
+                    order_item_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    order_id = table.Column<int>(type: "integer", nullable: true),
+                    part_id = table.Column<int>(type: "integer", nullable: true),
+                    quantity = table.Column<int>(type: "integer", nullable: true),
+                    unit_price = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: true),
+                    total_price = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_order_items", x => x.order_item_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_orders",
+                columns: table => new
+                {
+                    order_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    u_id = table.Column<int>(type: "integer", nullable: false),
+                    total_amount = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: true),
+                    status = table.Column<string>(type: "text", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_orders", x => x.order_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_part",
+                columns: table => new
+                {
+                    part_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    emp_id = table.Column<int>(type: "integer", nullable: false),
+                    part_name = table.Column<string>(type: "text", nullable: true),
+                    part_detail = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    part_price = table.Column<decimal>(type: "numeric(18,2)", precision: 18, scale: 2, nullable: false),
+                    part_stock = table.Column<int>(type: "integer", nullable: false),
+                    part_image = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    idt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_part", x => x.part_id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "tbl_user",
+                columns: table => new
+                {
+                    u_id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    u_name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    u_phone = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    u_email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    u_pass = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    u_role = table.Column<string>(type: "text", nullable: true),
+                    idt = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_tbl_user", x => x.u_id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "tbl_cart");
+
+            migrationBuilder.DropTable(
+                name: "tbl_emp");
+
+            migrationBuilder.DropTable(
+                name: "tbl_order_items");
+
+            migrationBuilder.DropTable(
+                name: "tbl_orders");
+
+            migrationBuilder.DropTable(
+                name: "tbl_part");
+
+            migrationBuilder.DropTable(
+                name: "tbl_user");
+        }
+    }
+}
