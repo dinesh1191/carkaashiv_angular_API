2a7f3301ab2200b66fe20d8f57322518746a17fa
dinesh_v <dinesh.varadhan10@gmail.com>
2026-05-28 17:07:05 +0530

    chore(program): Added frontend stagging link

--- Program.cs
+++ Program.cs
@@ -131,7 +131,8 @@ builder.Services.AddCors(options =>
     {
         policy.WithOrigins(
                 "http://localhost:4200", // local Angular app frontend URL
-                 "https://carkaashiv.netlify.app") // production frontend domain  url
+                 "https://carkaashiv.netlify.app",
+                 "https://deploy-preview-9--carkaashiv.netlify.app/") // production frontend domain  url
                    .AllowAnyHeader() // for security remove it later
                    .AllowAnyMethod() // get,post,put,update
                    .AllowCredentials(); // important for cookies
