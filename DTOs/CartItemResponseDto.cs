f5597e223c25845e3c6aec2dec84cc54339b6856
dinesh_v <dinesh.varadhan10@gmail.com>
2026-08-31 19:49:39 +0530

    fix(cartItemDto): PartName nullable property issue fixed.

--- DTOs/CartItemResponseDto.cs
+++ DTOs/CartItemResponseDto.cs
@@ -5,7 +5,7 @@
 
         public int CartId { get; set; }
         public int PartId { get; set; }
-        public string PartName { get; set; }
+        public string? PartName { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
         public decimal SubTotal { get; set; }
